48b526f7174f703450fdc1541e8aa18caf970826
Teesit Eadthongsai <teesit.e@draycir.com>
2020-02-17 14:18:18 +0700

    Support Azure DevOps Release with Current version number

--- services/azure-devops/azure-devops-base.js
+++ services/azure-devops/azure-devops-base.js
@@ -14,6 +14,32 @@ const latestBuildSchema = Joi.object({
     .required(),
 }).required()
 
+const latestReleaseSchema = Joi.object({
+  count: Joi.number().required(),
+  value: Joi.array()
+    .items(
+      Joi.object({
+        release: Joi.object({
+          id: Joi.number().required(),
+          artifacts: Joi.array().items(
+            Joi.object({
+              definitionReference: Joi.object({
+                version: Joi.object({
+                  name: Joi.string().required(),
+                }),
+              }),
+            }).required()
+          ),
+        }),
+        releaseEnvironment: Joi.object({
+          name: Joi.string().required(),
+        }),
+        deploymentStatus: Joi.string().required(),
+      })
+    )
+    .required(),
+}).required()
+
 module.exports = class AzureDevOpsBase extends BaseJsonService {
   static get auth() {
     return {
@@ -67,5 +93,81 @@ module.exports = class AzureDevOpsBase extends BaseJsonService {
     }
 
     return json.value[0].id
+  }
+
+  async getLatestCompletedBuildInfo(
+    organization,
+    project,
+    definitionId,
+    branch,
+    headers,
+    errorMessages
+  ) {
+    // Microsoft documentation: https://docs.microsoft.com/en-us/rest/api/azure/devops/build/builds/list?view=azure-devops-rest-5.0
+    const url = `https://dev.azure.com/${organization}/${project}/_apis/build/builds`
+    const options = {
+      qs: {
+        definitions: definitionId,
+        $top: 1,
+        statusFilter: 'completed',
+        'api-version': '5.0-preview.4',
+      },
+      headers,
+    }
+
+    if (branch) {
+      options.qs.branchName = `refs/heads/${branch}`
+    }
+
+    const json = await this.fetch({
+      url,
+      options,
+      schema: latestBuildSchema,
+      errorMessages,
+    })
+
+    if (json.count !== 1) {
+      throw new NotFound({ prettyMessage: 'build pipeline not found' })
+    }
+
+    return json.value[0]
+  }
+
+  async getLatestCompletedReleaseInfo(
+    organization,
+    project,
+    definitionId,
+    definitionEnvironmentId,
+    headers,
+    errorMessages
+  ) {
+    // Microsoft documentation: https://docs.microsoft.com/en-us/rest/api/azure/devops/build/builds/list?view=azure-devops-rest-5.0
+    const url = `https://vsrm.dev.azure.com/${organization}/${project}/_apis/release/deployments`
+    const options = {
+      qs: {
+        definitionId,
+        $top: 1,
+        definitionEnvironmentId,
+        'api-version': '5.0',
+      },
+      headers,
+    }
+
+    //if (branch) {
+    //  options.qs.sourceBranch = `refs/heads/${branch}`
+    //}
+
+    const json = await this.fetch({
+      url,
+      options,
+      schema: latestReleaseSchema,
+      errorMessages,
+    })
+
+    if (json.count !== 1) {
+      throw new NotFound({ prettyMessage: 'release pipeline not found' })
+    }
+
+    return json.value[0]
   }
 }
